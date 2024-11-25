a77bbaac57f399106ee9ba21bf0a71af42f8435f
dthanhff <dthanhff318@gmail.com>
2024-11-25 20:50:05 +0700

    update: change favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <link rel="icon" href="/bananaicon.png" />
       <body className={`${oswald.className}`}>{children}</body>
     </html>
   );
